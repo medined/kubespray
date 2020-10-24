b8cbd3efbd8bd86c918715e15cfc79fd720a933d
David Medinets <david.medinets@gmail.com>
2020-10-24 12:22:39 -0400

    Switch to Centos 7.

--- contrib/terraform/aws/variables.tf
+++ contrib/terraform/aws/variables.tf
@@ -21,19 +21,23 @@ variable "aws_cluster_name" {
 }
 
 data "aws_ami" "distro" {
+  owners      = ["679593333241"]
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+      name   = "name"
+      values = ["CentOS Linux 7 x86_64 HVM EBS *"]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+      name   = "architecture"
+      values = ["x86_64"]
   }
 
-  owners = ["099720109477"] # Canonical
+  filter {
+      name   = "root-device-type"
+      values = ["ebs"]
+  }
 }
 
 //AWS VPC Variables
